3fb03bf57c0197279ad6d199011f2502fafefed9
PGS-SOFT\phendzel <phendzel@pgs-soft.com>
2017-05-16 15:52:51 +0200

    Remove unnecessary dependencies

---
+++ src/test/java/com/project/java/prz/service/UserProjectServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.project.java.prz.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by phendzel on 5/16/2017.
+ */
+public class UserProjectServiceImplTest {
+
+    @Test
+    void getAll() throws Exception {
+        assertEquals(3,3);
+    }
+
+}
