eac1b0b869ca529bc551554ff000e43708890871
Piotr Hendzel <phendzel@pgs-soft.com>
2017-06-10 23:02:21 +0200

    fix tests

--- resource-server/src/test/java/com/project/java/prz/server/core/service/UserProjectServiceImplTest.java
+++ resource-server/src/test/java/com/project/java/prz/server/core/service/UserProjectServiceImplTest.java
@@ -1,43 +1,43 @@
 package com.project.java.prz.server.core.service;
 
 import com.project.java.prz.common.configuration.mockito.MockitoExtension;
-import com.project.java.prz.common.core.domain.general.Project;
-import com.project.java.prz.common.core.domain.general.UserDetail;
-import com.project.java.prz.common.core.domain.general.UserProject;
+import com.project.java.prz.common.core.domain.general.*;
+import com.project.java.prz.common.core.dto.SettingDTO;
 import com.project.java.prz.common.core.dto.UserDetailDTO;
 import com.project.java.prz.common.core.dto.UserProjectDTO;
+import com.project.java.prz.common.core.dto.UserSettingDTO;
 import com.project.java.prz.common.core.exception.UserProjectException;
 import com.project.java.prz.common.core.mapper.UserDetailMapper;
 import com.project.java.prz.common.core.mapper.UserProjectMapper;
+import com.project.java.prz.common.core.mapper.UserSettingMapper;
 import com.project.java.prz.server.core.repository.ProjectRepository;
 import com.project.java.prz.server.core.repository.UserProjectRepository;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import java.time.Clock;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
 /**
  * Created by phendzel on 5/26/2017.
  */
-@Disabled
 @ExtendWith(MockitoExtension.class)
 class UserProjectServiceImplTest {
 
     private static final Integer USER_PROJECT_ID = 1;
     private static final Integer PROJECT_ID = 11;
+    private static final Integer USER_SETTING_ID = 111;
+    private static final Integer SETTING_ID = 1111;
     private static final String USER_LOGIN = "user_login";
     private static final String USER_NAME = "Piotr";
     private static final Integer LABORATORY_GROUP = 2;
@@ -45,6 +45,7 @@ class UserProjectServiceImplTest {
     private static final String PROJECT_NAME = "Web application";
     private static final Integer AVAILABLE_PROJECTS_COUNTER = 5;
     private static final String PROJECT_DESCRIPTION = "Dummy description";
+    private static final String SETTING_VALUE = "2017-07-10";
 
     @Mock
     private Clock clock;
@@ -57,6 +58,9 @@ class UserProjectServiceImplTest {
 
     @Mock
     private UserProjectRepository userProjectRepository;
+
+    @Mock
+    private UserSettingService userSettingService;
 
     @InjectMocks
     private UserProjectService userProjectService = new UserProjectServiceImpl();
@@ -126,17 +130,23 @@ class UserProjectServiceImplTest {
 
     @Test
     void shouldDeleteByIdUsingStudentAccount() {
+        UserProject userProject = dummyUserProject();
+
         when(userService.getOne(USER_LOGIN)).thenReturn(dummyUserDetailsDTO());
-        when(userProjectRepository.findByUserDetailLogin(USER_LOGIN)).thenReturn(dummyUserProject());
+        when(userProjectRepository.findByUserDetailLogin(USER_LOGIN)).thenReturn(userProject);
         when(clock.instant()).thenReturn(DATETIME_OF_PROJECT_SELECTION.toInstant(ZoneOffset.UTC));
         when(clock.getZone()).thenReturn(ZoneOffset.UTC);
+        when(userSettingService.getUserSettingBySettingName(userProject.getUserDetail().getUserSettings(), SettingName.SCHEDULED_COMPLETION_DATE)).thenReturn(dummyUserSettingDTO());
         doNothing().when(userProjectRepository).delete(USER_PROJECT_ID);
 
         userProjectService.deleteById(USER_LOGIN, Collections.emptyList(), USER_PROJECT_ID);
 
         verify(userService, times(1)).getOne(USER_LOGIN);
         verify(userProjectRepository, times(1)).delete(USER_PROJECT_ID);
-        verifyNoMoreInteractions(userService, userProjectRepository);
+        verify(userProjectRepository, times(1)).findByUserDetailLogin(USER_LOGIN);
+        verify(userSettingService, times(1)).getUserSettingBySettingName(userProject.getUserDetail().getUserSettings(), SettingName.SCHEDULED_COMPLETION_DATE);
+        verify(userSettingService, times(1)).isAfterScheduledCompletionDateTime(LocalDate.parse(SETTING_VALUE));
+        verifyNoMoreInteractions(userService, userProjectRepository, userSettingService);
     }
 
     @Test
@@ -147,19 +157,23 @@ class UserProjectServiceImplTest {
         dummyUserProjectDTOReadyToUpdate.setMark("5.0");
         UserProject dummyUserProjectAfterUpdate = dummyUserProject();
         dummyUserProjectAfterUpdate.setAdditionalInformation(additionalInformation);
+        UserProject userProject = dummyUserProject();
 
         when(userService.getOne(USER_LOGIN)).thenReturn(dummyUserDetailsDTO());
-        when(userProjectRepository.findByUserDetailLogin(USER_LOGIN)).thenReturn(dummyUserProject());
+        when(userProjectRepository.findByUserDetailLogin(USER_LOGIN)).thenReturn(userProject);
         when(userProjectRepository.save(any(UserProject.class))).thenReturn(dummyUserProjectAfterUpdate);
         when(clock.instant()).thenReturn(DATETIME_OF_PROJECT_SELECTION.toInstant(ZoneOffset.UTC));
         when(clock.getZone()).thenReturn(ZoneOffset.UTC);
+        when(userSettingService.getUserSettingBySettingName(userProject.getUserDetail().getUserSettings(), SettingName.SCHEDULED_COMPLETION_DATE)).thenReturn(dummyUserSettingDTO());
 
         UserProjectDTO updatedUserProject = userProjectService.update(USER_LOGIN, Collections.emptyList(), dummyUserProjectDTOReadyToUpdate);
 
         assertNotNull(updatedUserProject);
         verify(userService, times(1)).getOne(USER_LOGIN);
-        verify(userProjectRepository, times(1)).save(any(UserProject.class));
-        verifyNoMoreInteractions(userService, userProjectRepository);
+        verify(userProjectRepository, times(1)).findByUserDetailLogin(USER_LOGIN);
+        verify(userSettingService, times(1)).getUserSettingBySettingName(userProject.getUserDetail().getUserSettings(), SettingName.SCHEDULED_COMPLETION_DATE);        verify(userProjectRepository, times(1)).save(any(UserProject.class));
+        verify(userSettingService, times(1)).isAfterScheduledCompletionDateTime(LocalDate.parse(SETTING_VALUE));
+        verifyNoMoreInteractions(userService, userProjectRepository, userSettingService);
     }
 
     private UserProjectDTO dummyUserProjectDTO() {
@@ -197,7 +211,29 @@ class UserProjectServiceImplTest {
     }
 
     private UserDetail dummyUserDetails() {
-        return UserDetailMapper.INSTANCE.convertToEntity(dummyUserDetailsDTO());
+        UserDetail userDetail = UserDetailMapper.INSTANCE.convertToEntity(dummyUserDetailsDTO());
+        userDetail.setUserSettings(dummyUserSettings());
+        return userDetail;
+    }
+
+    private UserSettingDTO dummyUserSettingDTO() {
+        UserSettingDTO userSettingDTO = new UserSettingDTO();
+        userSettingDTO.setId(USER_SETTING_ID);
+        userSettingDTO.setSettingDTO(dummySettingDTO());
+        userSettingDTO.setValue(SETTING_VALUE);
+        return userSettingDTO;
+    }
+
+    private SettingDTO dummySettingDTO() {
+        SettingDTO settingDTO = new SettingDTO();
+        settingDTO.setId(SETTING_ID);
+        settingDTO.setName(SettingName.SCHEDULED_COMPLETION_DATE);
+        return settingDTO;
+    }
+
+    private List<UserSetting> dummyUserSettings() {
+        UserSetting userSetting = UserSettingMapper.INSTANCE.convertToEntity(dummyUserSettingDTO());
+        return Collections.singletonList(userSetting);
     }
 
 }
